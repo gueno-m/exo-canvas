5ea7075d48e0936eab13dfcdf36928b404d8ddd4
gueno-m <gueno-m@users.noreply.github.com>
2020-09-23 16:41:23 +0200

    mouvement voiture+ nuages

--- index.js
+++ index.js
@@ -10,24 +10,29 @@ const moteur = new Audio('moteur.mp3')
 const klaxon = new Audio('klaxon.mp3')
 
 const cloud = new Image();
-cloud.src = 'https://upload.wikimedia.org/wikipedia/commons/7/73/W_cloud.svg';
+cloud.src = 'cloud.png';
 
-window.onload = function() {
+const crack = new Image();
+crack.src = 'crack.png';
+
+window.addEventListener("DOMContentLoaded", (event) => {
     moteur.play();
-};
-
+});
 
 let startX = 200;
 let startY = 100;
 
 let time = 0;
 let speed = 2;
+let input = 0;
+
+let isKlaxon = false
 
 function animation() {
     requestAnimationFrame(animation);
     time += speed
 
-    ctx.fillStyle = 'black'
+    ctx.fillStyle = 'grey'
     ctx.rect(0, 0, canvas.width, canvas.height);
     ctx.fill()
 
@@ -40,23 +45,30 @@ function animation() {
     // ctx.fillStyle = 'black'
     ctx.fill()
 
-    // for (let c = 1; c < 10; c++) {
-    //     ctx.beginPath();
-    //     ctx.drawImage(cloud, canvas.width / (Math.random() * c), horizon * (Math.random() * c), 300, 250);
-    //     ctx.fill()
-    // }
 
-    // cloud 
+    ctx.beginPath();
+    ctx.moveTo((time / 5 - 3) % canvas.width - 3, 40);
+    ctx.lineTo(time / 5 % canvas.width, 40);
+    ctx.strokeStyle = '#FFFFFF';
+    ctx.lineWidth = 2;
+    ctx.stroke();
 
-    // ctx.beginPath();
-    // ctx.moveTo(0, horizon);
-    // ctx.bezierCurveTo(0, 0, startX - 40, startY + 70, startX + 60, startY + 70);
-    // ctx.bezierCurveTo(startX + 80, startY + 100, startX + 150, startY + 100, startX + 170, startY + 70);
-    // ctx.bezierCurveTo(startX + 250, startY + 70, startX + 250, startY + 40, startX + 220, startY + 20);
-    // ctx.bezierCurveTo(startX + 260, startY - 40, startX + 200, startY - 50, startX + 170, startY - 30);
-    // ctx.bezierCurveTo(startX + 150, startY - 75, startX + 80, startY - 60, startX + 80, startY - 30);
-    // ctx.bezierCurveTo(startX + 30, startY - 75, startX - 20, startY - 60, startX, startY);
-    // ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo((time / 3 - 1) % canvas.width / 2 - 1, 60);
+    ctx.lineTo(time / 3 % canvas.width / 2, 60);
+    ctx.strokeStyle = '#FFFFFF';
+    ctx.lineWidth = 2;
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.drawImage(cloud, (0 + time) % canvas.width - 300, Math.sin(time / 100) * 10 + 100, 150, 100);
+    ctx.drawImage(cloud, (250 + time) % canvas.width - 300, Math.sin(time /
+        100) * 10 + 100, 150, 160);
+    ctx.drawImage(cloud, (470 + time) % canvas.width - 300, Math.sin(time / 100) * 10 + 100, 250, 150);
+    ctx.drawImage(cloud, (750 + time) % canvas.width - 300, Math.sin(time / 100) * 10 + 100, 160, 150);
+    ctx.drawImage(cloud, (1000 + time) % canvas.width - 300, Math.sin(time /
+        100) * 10 + 100, 300, 130);
+    ctx.fill()
 
     ctx.beginPath();
     ctx.moveTo(0, horizon);
@@ -86,12 +98,11 @@ function animation() {
 
         g = horizon + v;
         h = g + length * g * g * 0.0000002;
-        //Math.sqrt(length * i);
 
         if (h > canvas.height) {
             h = canvas.height;
         }
-        // console.log(g);
+
         ctx.beginPath();
         ctx.moveTo(canvas.width / 2, g);
         ctx.lineTo(canvas.width / 2, h);
@@ -99,29 +110,38 @@ function animation() {
         ctx.lineWidth = 5;
         ctx.stroke();
     }
+    o = time / 5 % 85.5
 
-    let value = navigator.getGamepads()[0].axes[2]
+    ctx.drawImage(crack, canvas.width / 2 + o, horizon + ((time / 85 * distance) % (canvas.height - horizon)), 60, 28)
 
-    // console.log(value)
+    let value = navigator.getGamepads()[0].axes[2];
 
-    if (value > 0.1) {
-        ctx.beginPath();
-        ctx.drawImage(car, canvas.width / 2, 600, 150, 90);
-        ctx.fill()
-    }
+    ctx.beginPath();
+    input = lerp(input, value, 0.1);
+    ctx.drawImage(car, canvas.width / 2 + input * 190 - 75, Math.sin(time / 20) * 2 + 600, 150, 90);
+    ctx.fill();
 
-    // if (value < -0.1) {
-    //     console.log("test2");
-    // }
-
-    if (navigator.getGamepads()[0].buttons[0].value = 1) {
-        klaxon.play()
+    if (navigator.getGamepads()[0].buttons[0].value == 1) {
+        if (isKlaxon = true) {
+            klaxon.play()
+            klaxon.loop = false
+        }
+        isKlaxon = true
     }
     //console.log(navigator.getGamepads()[0].buttons[0].pressed)
 }
 
+document.addEventListener("keydown", function(e) {
+    if (e.keyCode === 37) {
+        console.log("gauche")
+    } else if (e.keyCode === 39) {
+        console.log("droite")
+    }
+});
+
 window.addEventListener("gamepadconnected", (event) => {
     console.log("A gamepad connected:");
+    animation();
 });
 
 window.addEventListener("gamepaddisconnected", (event) => {
@@ -129,4 +149,8 @@ window.addEventListener("gamepaddisconnected", (event) => {
     console.log(event.gamepad);
 });
 
-animation();
+function lerp(value1, value2, amount) {
+    amount = amount < 0 ? 0 : amount;
+    amount = amount > 1 ? 1 : amount;
+    return value1 + (value2 - value1) * amount;
+}
